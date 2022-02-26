d85126cff6b7ad1d567972634fa40707dac373c9
Sadykov Daniil <sad1dani@mail.ru>
2022-02-26 20:27:38 +0300

    Добавьте файлы проекта.

---
+++ NeuralNetwork/Pictures.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace NeuralNetwork
+{
+    class Pictures
+    {
+        static int Height, Width;
+
+        public static byte[] ImageToByte(Image img)
+        {
+            var bmp = new Bitmap(img);
+            var bytes = new byte[bmp.Width * bmp.Height];
+            Height = img.Height;
+            Width = img.Width;
+
+            int i = 0;
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    var IsWhite = bmp.GetPixel(x, y).R >= 230 &&
+                        bmp.GetPixel(x, y).G >= 230 && bmp.GetPixel(x, y).B >= 230;
+                    bytes[i++] = (byte)(IsWhite ? 0 : 1);
+                }
+            }
+
+            return bytes;
+        }
+
+        public static void PrintPicturesConsole(byte[] picture)
+        {
+            int i = 0;
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (picture[i++] == 1)
+                        Console.Write("1");
+                    else
+                        Console.Write("0");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace NeuralNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pictures = new List<byte[]>()
+            {
+                Pictures.ImageToByte(new Bitmap(@"C:\Users\sad1d\OneDrive\Изображения\Picture1.bmp")),
+                Pictures.ImageToByte(new Bitmap(@"C:\Users\sad1d\OneDrive\Изображения\Picture2.bmp")),
+                Pictures.ImageToByte(new Bitmap(@"C:\Users\sad1d\OneDrive\Изображения\Picture3.bmp")),
+                Pictures.ImageToByte(new Bitmap(@"C:\Users\sad1d\OneDrive\Изображения\Picture4.bmp")),
+            };
+
+            Pictures.PrintPicturesConsole(pictures[0]);
+
+
+
+            // массив входных обучающих векторов
+            Vector[] X = {
+                new Vector(0, 0),
+                new Vector(0, 1),
+                new Vector(1, 0),
+                new Vector(1, 1)
+            };
+
+                        // массив выходных обучающих векторов
+            //Vector[] Y = {
+            //    new Vector(0.0), // 0 ^ 0 = 0
+            //    new Vector(1.0), // 0 ^ 1 = 1
+            //    new Vector(1.0), // 1 ^ 0 = 1
+            //    new Vector(0.0) // 1 ^ 1 = 0
+            //};
+
+            //Network network = new Network(new int[] { 2, 4, 1 }); // создаём сеть с двумя входами, тремя нейронами в скрытом слое и одним выходом
+
+            //network.Train(X, Y, 0.8, 1e-3, 5000); // запускаем обучение сети 
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Vector output = network.Forward(X[i]);
+            //    Console.WriteLine($"X: {X[i][0]} {X[i][1]}, Y: {Y[i][0]}, output: {output[0]}");
+            //}
+
+            Console.ReadKey();
+        }
+
+    }
+}
